3c00936dedbcb7c0c5864f1b83fe94fae1250694
neldevfull <nfrugeri@gmail.com>
2016-04-14 16:49:21 +0000

    Improvement chat

--- main.js
+++ main.js
@@ -14,42 +14,69 @@ $(function() {
             break;
 
         case "chat.html":
-            $("#userName").empty().append(localStorage.getItem("fullname"));
+            var userName = localStorage.getItem("fullname");
+            $("#userName").empty().append(userName);
 
-            var allQuestions = JSON.parse(localStorage.getItem("allQuestions"));
+            var initial = function() {
+                var allQuestions = JSON.parse(localStorage.getItem("allQuestions"));
 
-            var list = "";
-            if(allQuestions) {
-                allQuestions.reverse().forEach( function(element, index) {
-                    list += "<li>"+element.questions+"</li>"+
-                        "<li>"+element.response+"</li>";
-                });
+                var list = "";
+                if(allQuestions) {
+                    for (var i = 0; i < allQuestions.length; i++) {
+                        var respond = i == 0 ? allQuestions[i].response :
+                            allQuestions[i].respTwo;
+
+                        list += "<li>"+allQuestions[i].userName+"</li>"+
+                            "<li>"+allQuestions[i].questions+"</li>"+
+                            "<li>"+allQuestions[i].consultant+"</li>"+
+                            "<li>"+respond+"</li>";
+                    }
+                }
+
+                $("#displayQuestions").empty().append(list);
             }
 
-            $("#displayQuestions").empty().append(list);
+            initial();
 
             $("#sendChat").on("click", function() {
+                var responseOne = "Olá "+userName+" nós estamos muito felizes em atendê-lo."+
+                    "O MEI estará dispensado de emitir nota fiscal para consumidor pessoa"+
+                    "física, porém, estará obrigado à emissão quando o destinatário da "+
+                    "mercadoria ou serviço for outra empresa, salvo quando esse destinatário "+
+                    "for o emissor da nota fiscal de entrada.";
+
+                var responseTwo = userName+" tenha um bom dia e disponha do nosso serviço de "+
+                    "atendimento online através do nosso chat";
+
                 var allQuestions = [];
                 var question = {
+                    userName: userName,
+                    consultant: "Laura C. Consultora",
                     questions: $("#question").val(),
-                    response: "Ola, nós estamos muito felizes"
+                    response: responseOne,
+                    respTwo: responseTwo
                 };
 
                 if(JSON.parse(localStorage.getItem("allQuestions")))
                     allQuestions = JSON.parse(localStorage.getItem("allQuestions"));
 
-                allQuestions.push(question);
+                var quest = '<li style="color:blue">'+question.userName+'<li>'
+                    +question.questions+'</li></li>';
+                var res   = "";
 
-                var question = "<li>"+question.questions+"</li>";
-                var response = "<li>"+question.response+"</li>";
+                if(allQuestions.length > 0)
+                    res = "<li>"+question.consultant+"</li><li>"+question.respTwo+"</li>";
+                else
+                    res = "<li>"+question.consultant+"</li><li>"+question.response+"</li>";
 
                 $("#question").val("");
-                $("#displayQuestions").append(question);
+                $("#displayQuestions").append(quest);
 
                 setTimeout(function() {
-                    $("#displayQuestions").append(response);
+                    $("#displayQuestions").append(res);
                 }, 4000);
 
+                allQuestions.push(question);
                 localStorage.setItem("allQuestions", JSON.stringify(allQuestions));
             });
 
